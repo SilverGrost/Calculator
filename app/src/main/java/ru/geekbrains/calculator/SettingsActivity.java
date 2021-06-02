44b00a1630f2a0a78835a9c4be29f1cbcb89ba70
SilverGrost <silvergrost@silcomsoft.ru>
2021-06-02 14:49:43 +0300

    Добавил обработку активити с настроками

--- app/src/main/java/ru/geekbrains/calculator/SettingsActivity.java
+++ app/src/main/java/ru/geekbrains/calculator/SettingsActivity.java
@@ -2,13 +2,68 @@ package ru.geekbrains.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
 
-public class SettingsActivity extends AppCompatActivity {
+import ru.geekbrains.calculator.ui.Constants;
+import ru.geekbrains.calculator.ui.MainActivity;
+
+public class SettingsActivity extends AppCompatActivity implements View.OnClickListener, Constants {
+
+    private int radioBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // получить данные из Intent
+        radioBtn = getIntent().getExtras().getInt(intentParam);
+        setTheme(MainActivity.rbToStyleID(radioBtn));
         setContentView(R.layout.activity_settings);
+        initView(radioBtn);
+    }
+
+    // Инициализируем все вьюхи
+    private void initView(int rb) {
+        RadioButton radioButtonMyStyle = findViewById(R.id.radioButton6);
+        RadioButton radioButtonLightStyle = findViewById(R.id.radioButton7);
+        RadioButton radioButtonDarkStyle = findViewById(R.id.radioButton8);
+        Button buttonFinish = findViewById(R.id.button_finish);
+
+        radioButtonMyStyle.setOnClickListener(this);
+        radioButtonLightStyle.setOnClickListener(this);
+        radioButtonDarkStyle.setOnClickListener(this);
+        buttonFinish.setOnClickListener(this);
+
+        switch (rb) {
+            case 0:
+                radioButtonMyStyle.setChecked(true);
+                break;
+            case 1:
+                radioButtonLightStyle.setChecked(true);
+                break;
+            default:
+                radioButtonDarkStyle.setChecked(true);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.radioButton6) {//applyTheme(R.style.AppThemeMy);
+            radioBtn = AppThemeMy;
+        } else if (id == R.id.radioButton7) {//applyTheme(R.style.AppThemeLight);
+            radioBtn = AppThemeLight;
+        } else if (id == R.id.radioButton8) {//applyTheme(R.style.AppThemeDark);
+            radioBtn = AppThemeDark;
+        } else if (id == R.id.button_finish) {
+            Intent intentResult = new Intent();
+            intentResult.putExtra(intentParam, radioBtn);
+            setResult(RESULT_OK, intentResult);
+
+            finish();
+        }
     }
 }
--- app/src/main/java/ru/geekbrains/calculator/ui/Constants.java
+++ app/src/main/java/ru/geekbrains/calculator/ui/Constants.java
@@ -5,4 +5,8 @@ public interface Constants {
     int AppThemeMy = 0;
     int AppThemeLight = 1;
     int AppThemeDark = 2;
+    String dateForSave = "dateForSave";
+    String NameSharedPreference = "THEME";
+    String AppTheme = "APP_THEME";
+    int REQUEST_CODE_SETTING_ACTIVITY = 99;
 }
