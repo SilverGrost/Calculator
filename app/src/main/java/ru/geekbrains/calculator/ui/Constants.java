edc3fcca6ed318bb912df255d554b4027f261be8
SilverGrost <silvergrost@silcomsoft.ru>
2021-06-02 14:11:01 +0300

    Добавил активити с настройками

---
+++ app/src/main/java/ru/geekbrains/calculator/ui/Constants.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.calculator.ui;
+
+public interface Constants {
+    String intentParam = "rb";
+    int AppThemeMy = 0;
+    int AppThemeLight = 1;
+    int AppThemeDark = 2;
+}
